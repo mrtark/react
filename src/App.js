4896eee98772571d78fc6ff0d56df2aecda26081
mrtark <murat_ark97@hotmail.com>
2023-02-11 16:12:19 +0300

    router,switch

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './component/Header';
 import Main from './component/Main';
 import Footer from './component/Footer';
+import { BrowserRouter as Route, Router, Switch } from 'react-router-dom';
+import React from 'react';
+import RegisterList from './component/register crud page/RegisterList';
+import RegisterCreate from './component/register crud page/RegisterCreate';
+import RegisterDetail from './component/register crud page/RegisterDetail';
 
 
 function App() {
   return (
-    <>
-    <Header/>
-    <Main/>
-    <Footer/>
-    </>
+    <React.Fragment>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={RegisterList}></Route>
+          <Route path="/register-list" component={RegisterList}></Route>
+          <Route path="/register-add/:id" component={RegisterCreate}></Route>
+          <Route path="/register-detail/:id" component={RegisterDetail}></Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </React.Fragment>
   );
 }
 
