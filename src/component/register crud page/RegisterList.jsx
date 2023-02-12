da7f7ce0a6bf18f7616187f0a372607c240038ba
mrtark <murat_ark97@hotmail.com>
2023-02-12 17:11:11 +0300

    Blog project CDM api call

--- src/component/register crud page/RegisterList.jsx
+++ src/component/register crud page/RegisterList.jsx
@@ -1,6 +1,27 @@
 import React, { Component } from 'react'
+import Register from '../../services/Register';
 
 export default class RegisterList extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      registerList: [],
+    }
+  }
+
+  componentDidMount() {
+    Register.listApiRegister().then(
+      (reply) => {
+        this.setState(
+          {
+            registerList:reply.data
+          }
+        )
+      }
+    )
+  }
+
   render() {
     return (
       <>RegisterList</>
