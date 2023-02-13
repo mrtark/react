3e4a9a578cd171399dc838ef666ed28f4672686a
mrtark <murat_ark97@hotmail.com>
2023-02-13 14:27:35 +0300

    data listed

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -3,7 +3,32 @@ import React from 'react'
 export default function Footer() {
   return (
     <>
-    Footer
+      <footer className='bg-dark fixed-bottom'>
+
+        <section className="ftr">
+          <div className="text-center text-dark mt-4 pt-4">
+            <ul>
+              <a href="#">Anasayfa</a>
+              <a href="#">Blog</a>
+              <a href="#">Kategori</a>
+              <a href="#">İletişim</a>
+            </ul>
+
+          </div>
+          <div className="text-center">
+            <ul className="ftr-social">
+              <a href="#"><i className="fa-brands fa-github"></i> Github</a>
+              <a href="#"><i className="fa-brands fa-linkedin"></i> Linkedın</a>
+              <a href="#"><i className="fa-brands fa-instagram"></i> Instagram</a>
+              <a href="#"><i className="fa-brands fa-twitter"></i> Twitter</a>
+            </ul>
+          </div>
+        </section>
+        <div className="text-center text-white  bg-dark fw-light" id="copy_right">
+          2021 - <span id="copy_date"></span> <br />
+          &copy; Tüm Hakları Saklıdır. <br />
+        </div>
+      </footer>
     </>
   )
 }
--- src/component/register crud page/RegisterList.jsx
+++ src/component/register crud page/RegisterList.jsx
@@ -15,7 +15,7 @@ export default class RegisterList extends Component {
       (reply) => {
         this.setState(
           {
-            registerList:reply.data
+            registerList: reply.data
           }
         )
       }
@@ -24,7 +24,51 @@ export default class RegisterList extends Component {
 
   render() {
     return (
-      <>RegisterList</>
+      <>
+        <section className="container tabless mt-4">
+          <div className="card girilenİslemList shadow">
+            <div className="card-header text-center bg-baslık">
+              <h3>Kullanıcı Listesi</h3>
+            </div>
+            <table className="table table-hover table-striped table-sm text-center" id="datalistregisterTable">
+              <thead>
+                <tr className="text-center text-white bg-dark">
+                  <th>ID</th>
+                  <th>ADI</th>
+                  <th>SOYADI</th>
+                  <th>EMAİL</th>
+                  <th>ŞİFRE</th>
+                  <th>TELEFON</th>
+                  <th>Tarih ve Saat</th>
+                  <th>Güncelle</th>
+                  <th>Detay</th>
+                  <th>Sil</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                {
+                  this.state.registerList.map(temp=>
+                    <tr key={temp}>
+                      <td>{temp.id}</td>
+                      <td>{temp.username}</td>
+                      <td>{temp.surname}</td>
+                      <td>{temp.email}</td>
+                      <td>{temp.passwd}</td>
+                      <td>{temp.telephoneNumber}</td>
+                      <td>{temp.createdDate}</td>
+                      <td><i class="fa-solid fa-file-pen text-dark fs-4"></i></td>
+                      <td><i class="fa-solid fa-circle-info text-dark fs-4"></i></td>
+                      <td><i class="fa-solid fa-trash-can text-dark fs-4"></i></td>
+                      
+                    </tr>
+                  )
+                }
+              </tbody>
+            </table>
+          </div>
+        </section>
+      </>
     )
   }
 }
